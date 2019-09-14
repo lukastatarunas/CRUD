09592612bc642588d9094cd47cb61cedd8d54bc2
lukastatarunas <lukasttatarunas@gmail.com>
2019-09-14 14:45:38 +0300

    Updates

---
+++ precache-manifest.e76b318ebe520bf4961d7a60d38bccae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d896d922f997e818486dcd694d951ab",
+    "url": "/CRUD/index.html"
+  },
+  {
+    "revision": "ad33c8f594720fb1f93b",
+    "url": "/CRUD/static/css/main.5ca82edb.chunk.css"
+  },
+  {
+    "revision": "49426fabde240fa345e6",
+    "url": "/CRUD/static/js/2.4ab52aad.chunk.js"
+  },
+  {
+    "revision": "ad33c8f594720fb1f93b",
+    "url": "/CRUD/static/js/main.85f794ac.chunk.js"
+  },
+  {
+    "revision": "9a8d74cc57605bf0b5e0",
+    "url": "/CRUD/static/js/runtime~main.eb419fda.js"
+  }
+]);
